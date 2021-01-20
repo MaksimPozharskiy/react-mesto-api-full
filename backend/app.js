c5e536b6838fd6243891879092d15843fd712d2b
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2021-01-21 00:00:11 +0300

    feat: добавить мидлвэр авторизации в app

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+
 const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -27,10 +29,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/', usersRouter);
-app.use('/', cardsRouter);
 app.post('/signin', login);
 app.post('/signup', createUser);
+// авторизация
+app.use(auth);
+app.use('/', usersRouter);
+app.use('/', cardsRouter);
 app.use('*', (req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
 
 app.listen(PORT, () => {
