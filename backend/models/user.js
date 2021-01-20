5514d83a70a79b816277de4bf40b35142c02dee2
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2021-01-20 22:14:37 +0300

    refactor: в модели User сделать имя, описание и ссылку необязательными полями

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,12 +7,14 @@ const userSchema = new mongoose.Schema({
     required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
@@ -23,6 +25,7 @@ const userSchema = new mongoose.Schema({
       },
       message: 'Ссылка не в корректном формате',
     },
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
