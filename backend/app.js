15379d855b1672ae4e91074010a28c4cfa182c96
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2021-01-20 23:33:06 +0300

    feat: написать signin, signup роуты

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+const { createUser, login } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -28,6 +29,8 @@ app.use((req, res, next) => {
 
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
+app.post('/signin', login);
+app.post('/signup', createUser);
 app.use('*', (req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
 
 app.listen(PORT, () => {
