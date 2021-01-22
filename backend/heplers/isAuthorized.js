d691babeb52f9ddb02e471ee86d4d14d754e669f
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2021-01-22 23:18:16 +0300

    refactor: исправить try catch в isAuthorized согласно правилам eslint

--- backend/heplers/isAuthorized.js
+++ backend/heplers/isAuthorized.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 
 const { JWT_SECRET } = process.env;
 
-const isAuthorized = token => {
+const isAuthorized = (token) => {
   try {
     return jwt.verify(token, JWT_SECRET);
-  } catch {
+  } catch (err) {
     return false;
   }
 };
