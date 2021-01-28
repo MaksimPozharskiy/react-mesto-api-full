9aabe7c2eb3c16459f29d9a5a863bf1d22ec7de7
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2021-01-28 19:52:45 +0300

    hotfix: переписать мидлвару 404 под централизованную обработку ошибок

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const cardsRouter = require('./routes/cards');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const NotFoundError = require('./errors/not-found-err');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -56,7 +57,9 @@ app.post('/signin', celebrate({
 app.use(auth);
 app.use('/users', usersRouter);
 app.use('/', cardsRouter);
-app.use('*', (req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
+app.use('*', () => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
 app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res) => {
